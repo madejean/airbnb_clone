1b647f11be0b50aa9b0b22b44fcc5c9d5e5eb8d3
madejean <madejean007@hotmail.fr>
2016-07-12 23:52:08 +0000

    added variables

--- api/config.py
+++ api/config.py
@@ -1,20 +1,23 @@
 import os
 env_name = os.environ.get('AIRBNB_ENV')
+environment = ['developement', 'production']
+database = { enviroment[0]:'airbnb_dev', environment[1]:'airbnb_prod' }
 
 if env_name == None:
     print "Please set AIRBNB_ENV environment variable to either produce or develop"
     quit()
-elif env_name != "development" or env_name != "production":
+elif env_name != "development" and env_name != "production":
     print "AIRBNB_ENV environment variable has an usupported value"
     quit()
 
-if env_name == 'development':
+if env_name == environment[0]:
     DEBUG = True
     HOST = 'localhost'
     PORT = '3333'
     DATABASE = {
         'host': '158.69.77.113',
         'user': 'airbnb_user_dev',
+        'database': database[environment[0]],
         'port': '3306',
         'charset': 'utf8',
         'password': os.environ.get('AIRBNB_DATABASE_PWD_DEV'),
@@ -27,6 +30,7 @@ else:
     DATABASE = {
         'host': '158.69.77.113',
         'user': 'airbnb_user_prod',
+        'database': database[environment[1]],
         'port': '3306',
         'charset': 'utf8',
         'password': os.environment.get('AIRBNB_DATABASE_PWD_PROD'),
